4b95bd55744f7e37b44cc79f9ed13c1b9204c8aa
oskarkraemer <42745862+oskarkraemer@users.noreply.github.com>
2020-04-14 22:32:36 +0200

    Add files via upload
    initial commit

---
+++ sims4needs - public.py
@@ -0,0 +1,21 @@
+from PIL import ImageGrab, Image
+import requests, json
+from rgbxy import Converter
+import time
+
+converter = Converter()
+
+bridge_ip= ""
+key = ""
+
+room_id = ""
+grab_xy = [78,1404]
+
+while 1:
+    im=ImageGrab.grab()
+    pix= im.load()
+    pixrgb=pix[grab_xy[0],grab_xy[1]]
+    converter.rgb_to_xy(pixrgb[0],pixrgb[1],pixrgb[2])[0]
+    data ={'xy':[converter.rgb_to_xy(pixrgb[0],pixrgb[1],pixrgb[2])[0],converter.rgb_to_xy(pixrgb[0],pixrgb[1],pixrgb[2])[1]]}
+    r =requests.put("http://"+bridge_ip+"/api/"+key+"/groups/"+room_id+"/action",json.dumps(data))
+    time.sleep(5)
